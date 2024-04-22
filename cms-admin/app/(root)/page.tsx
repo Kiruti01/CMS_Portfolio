ceac6f14a4dc3ea86e2a77027d5731860bd40ca5
Michael Kiruti <mikekirutic@gmail.com>
2024-04-22 15:44:15 +0300

    modal components

--- cms-admin/app/(root)/page.tsx
+++ cms-admin/app/(root)/page.tsx
@@ -1,30 +1,20 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-// import { Modal } from "@/components/ui/modal";
-// import { useStoreModal } from "@/hooks/use-store-modal";
-// import { useEffect } from "react";
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const SetupPage = () => {
-  // const onOpen = useStoreModal((state) => state.onOpen);
-  // const isOpen = useStoreModal((state) => state.isOpen);
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
 
-  // useEffect(() => {
-    // if(!isOpen){
-      // onOpen();
-    // }
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
 
-  // }, [isOpen, onOpen]);
-
-  return (
-    <div className="p-4">
-      <UserButton afterSignOutUrl="/" />
-   
-    </div>
-  );
-}
+  return null;
+};
 
 export default SetupPage;
-
-  
--- cms-admin/app/layout.tsx
+++ cms-admin/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/providers/modal-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ModalProvider />
 
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
