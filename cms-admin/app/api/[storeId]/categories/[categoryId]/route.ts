b911a2746195698504a9f1c2e072c92510df9b04
Michael Kiruti <mikekirutic@gmail.com>
2024-04-25 12:21:28 +0300

    categories

--- cms-admin/app/api/[storeId]/categories/[categoryId]/route.ts
+++ cms-admin/app/api/[storeId]/categories/[categoryId]/route.ts
@@ -15,11 +15,7 @@ export async function GET(
       where: {
         id: params.categoryId,
       },
-      include: {
-        billboard: true,
-      },
     });
-
     return NextResponse.json(category);
   } catch (error) {
     console.log("[CATEGORY_GET", error);
